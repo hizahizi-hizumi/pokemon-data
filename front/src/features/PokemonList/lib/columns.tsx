90c69d07a39efc160433d3053263ea6f9d1bd2c0
hizahizi-hizumi <hizahizihizumi@gmail.com>
2024-08-11 18:31:24 +0900

    feat: 画像セルの見た目を調整

--- front/src/features/PokemonList/lib/columns.tsx
+++ front/src/features/PokemonList/lib/columns.tsx
@@ -1,6 +1,20 @@
-import type { GridColDef } from "@mui/x-data-grid";
+import type { GridColDef, GridRenderCellParams } from "@mui/x-data-grid";
+
+import { ImageCell } from "@/features/PokemonList/components/ImageCell";
+import type { Row } from "@/features/PokemonList/types/row";
 
 export const columns: GridColDef[] = [
+  {
+    field: "image",
+    headerName: "",
+    align: "center",
+    disableColumnMenu: true,
+    sortable: false,
+    width: 50,
+    renderCell: (params: GridRenderCellParams<Row>) => (
+      <ImageCell url={params.value} />
+    ),
+  },
   {
     field: "id",
     headerName: "番号",
@@ -8,12 +22,5 @@ export const columns: GridColDef[] = [
   {
     field: "name",
     headerName: "名前",
-  },
-  {
-    field: "image",
-    headerName: "画像",
-    renderCell: (params) => {
-      return <img src={params.value} alt="" />;
-    },
   },
 ];
