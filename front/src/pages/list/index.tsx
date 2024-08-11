7c849c5091858faac318be10162a891af2301ab0
hizahizi-hizumi <hizahizihizumi@gmail.com>
2024-08-11 11:44:29 +0900

    feat: ポケモンを一覧表示するルーティング作成

---
+++ front/src/pages/list/index.tsx
@@ -0,0 +1,11 @@
+import type React from "react";
+
+import { PokemonList } from "@/features/PokemonList/PokemonList";
+
+export default function List(): React.JSX.Element {
+  return (
+    <>
+      <PokemonList />
+    </>
+  );
+}
