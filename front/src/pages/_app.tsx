bfb3db86930ad8000c425a063c45e8baef3f5656
hizahizi-hizumi <hizahizihizumi@gmail.com>
2024-08-11 19:10:56 +0900

    feat: サイト全体の見た目の調整

--- front/src/pages/_app.tsx
+++ front/src/pages/_app.tsx
@@ -4,11 +4,14 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import { useTheme } from "@mui/material";
 
 export default function App() {
+  const theme = useTheme()
+
   return (
     <>
-      <main>
+      <main style={{ height: "100vh", width: "100%", padding: theme.spacing(1) }}>
         <Outlet />
       </main>
     </>
